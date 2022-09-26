cece033ae418edeabe51e1e63ae4b9f2f8426433
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2022-09-26 14:51:25 -0500

    Begin Making Noise into Objects

---
+++ Runtime/OggalNoise.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = " New Oggal Noise",menuName ="Terrain Noise/Oggal Noise")]
+public class OggalNoise : TerrainNoiseObject
+{
+    TerrainNoise myNoise;
+    public override bool isValid {get => (myNoise != null);}
+
+    public override float getHeight(Vector2 pos){
+
+        return myNoise.getHeight(pos.x,pos.y);
+    }
+
+    public override void Intialize(int Seed, float Scale)
+    {
+        myNoise = new TerrainNoise(Seed,Mathf.RoundToInt(Scale));
+    }
+
+
+}
---
+++ Runtime/TerrainNoiseObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TerrainNoiseObject : ScriptableObject
+{
+    public abstract bool isValid{get;}
+    public abstract float getHeight(Vector2 pos);
+
+    public abstract void Intialize(int Seed, float Scale);
+}
