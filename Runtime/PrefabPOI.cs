c885277c0d422d868cb167d5169d65407061d8cd
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2022-11-02 12:14:05 -0500

    Add PrefabPOI
    Adds Sample POI as a PrefabPOI ScriptableObject

---
+++ Runtime/PrefabPOI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = "New POI", menuName ="Terrain Noise/Modifiers/POI")]
+public class PrefabPOI : TerrainNoiseModifier
+{
+    
+    [SerializeField] Vector3 Position;
+    [SerializeField] GameObject Prefab;
+    [SerializeField] float innerRadius, outerRadius;
+
+    public override float GetRatio(float pointX, float pointY)
+    {
+        float distance = Vector2.Distance(new Vector2(Position.x,Position.z), new Vector2(pointX, pointY));
+        if (distance > outerRadius + innerRadius)
+        {
+            return 0;
+        }
+        else if(distance < innerRadius)
+        {
+            return 1;
+        }
+        return 1-(distance - innerRadius) / (outerRadius);
+    }
+
+    public override float GetTargetHeight(float pointX, float pointY)
+    {
+        return Position.y;
+    }
+}
