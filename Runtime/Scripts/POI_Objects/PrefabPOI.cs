0fc61d91f7f31d69444a133a31fa6ba0f05fae60
Philip Taylor - Laptop <philip.dan.taylor@gmail.com>
2023-01-16 14:53:52 -0600

    Minor Style Corrections

--- Runtime/Scripts/POI_Objects/PrefabPOI.cs
+++ Runtime/Scripts/POI_Objects/PrefabPOI.cs
@@ -12,7 +12,7 @@ public class PrefabPOI : TerrainNoiseModifier
 
     public override float GetRatio(float pointX, float pointY)
     {
-        float distance = Vector2.Distance(new Vector2(Position.x,Position.z), new Vector2(pointX, pointY));
+        float distance = Vector2.Distance(new Vector2(Position.x, Position.z), new Vector2(pointX, pointY));
         if (distance > outerRadius + innerRadius)
         {
             return 0;
@@ -21,7 +21,7 @@ public class PrefabPOI : TerrainNoiseModifier
         {
             return 1;
         }
-        return 1-(distance - innerRadius) / (outerRadius);
+        return 1 - (distance - innerRadius) / (outerRadius);
     }
 
     public override float GetTargetHeight(float pointX, float pointY)
